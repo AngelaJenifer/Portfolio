b93e0412822616f5da2b2344d2bebcb96020f62a
Angela-1302 <angelajenifer@d-sat.com>
2026-07-03 10:47:50 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -208,8 +208,7 @@ const isDark = theme === "dark";
               </div>
               </div>
              
-            </div>
-            
+
             {/* Desktop Menu */}
             <div className="hidden md:flex items-center space-x-8">
               {['Home', 'About', 'Experience', 'Projects', 'Contact'].map((item) => (
